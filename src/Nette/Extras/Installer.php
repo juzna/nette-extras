398ed463aeb9369b4c52253166e9b0ee308d718d
Jan Dolecek <juzna.cz@gmail.com>
2012-08-01 13:05:43 +0200

    more installers

--- src/Nette/Extras/Installer.php
+++ src/Nette/Extras/Installer.php
@@ -23,13 +23,30 @@ class Installer implements \Nette\Addons\CustomInstallers\IInstaller
 
 	public function install(InstalledRepositoryInterface $repo, PackageInterface $package, $section, & $config)
 	{
-		var_dump(getcwd(), $this->baseDir);
+		// var_dump(getcwd(), $this->baseDir);
 
 		$items = (array) $section; // expected list of items to be installed
+
 
 		if (in_array('jQuery', $items)) {
 			copy("$this->baseDir/assets/javascript/jquery.js", "www/temp/jquery.js"); // copy to output
 			$config['assets']['javascript'][] = 'temp/jquery.js'; // add to addons.neon config
+		}
+
+
+		if (in_array('jQuery-UI', $items)) {
+			`cp -r $this->baseDir/assets/css/jquery-ui www/temp/jquery-ui`; // FIXME: do it by PHP
+			copy("$this->baseDir/assets/javascript/jquery-ui.js", "www/temp/jquery-ui.js"); // copy to output
+
+			// add to addons.neon config
+			$config['assets']['css'][] = 'temp/jquery-ui/jquery-ui.css';
+			$config['assets']['javascript'][] = 'temp/jquery-ui.js';
+		}
+
+
+		if (in_array('netteForms', $items)) {
+			copy("$this->baseDir/assets/javascript/netteForms.js", "www/temp/netteForms.js"); // copy to output
+			$config['assets']['javascript'][] = 'temp/netteForms.js'; // add to addons.neon config
 		}
 
 	}
